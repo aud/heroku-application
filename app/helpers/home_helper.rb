c1306dcf309eb46d5f97dfbf198f986b033ceb49
Elliot Dohm <elliot.dohm@shopify.com>
2016-10-19 10:20:06 -0700

    Code tabs

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,7 +1,7 @@
 module HomeHelper
 	def logging
 		if Rails.env == 'production'
-		  @logs = `tail -n 50 log/production.log`.split(/\n/)
+		  @logs = `tail -n 25 log/production.log`.split(/\n/)
 			render text: @logs
 		else
 			@logs = `tail -n 50 log/development.log`.split(/\n/)
