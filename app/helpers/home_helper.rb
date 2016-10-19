fdc6d45e7e83fb4aa3d32be7f7ded89c170e32a6
Elliot Dohm <elliot.dohm@shopify.com>
2016-10-18 23:18:37 -0700

    Add verbose logging view

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,2 +1,11 @@
 module HomeHelper
+	def logging
+		if Rails.env == 'production'
+		  @logs = `tail -n 50 log/production.log`.split(/\n/)
+			render text: @logs
+		else
+			@logs = `tail -n 50 log/development.log`.split(/\n/)
+			render text: @logs
+		end
+	end
 end
